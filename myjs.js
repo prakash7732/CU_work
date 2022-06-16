df775d422d43a07941f50536c9bf9ed5b598a174
swa prakash <104055279+prakash7732@users.noreply.github.com>
2022-06-16 03:48:44 -0700

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,13 @@
+function onit(){
+    console.log("working!!!!!!!!!!")
+    document.getElementById("myimg").src="ONbulb.jpg";
+
+    
+}
+
+function offit(){
+    console.log("working!!!!!!!!!!")
+    document.getElementById("myimg").src="OFFbulb.jpg";
+
+    
+}
